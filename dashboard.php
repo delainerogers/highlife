0fcc59b2427afd7389296fcc31cc13281bf4af28
rishab2113 <rishab_nayak@yahoo.com>
2017-10-29 02:23:08 -0400

    nm

--- dashboard.php
+++ dashboard.php
@@ -9,7 +9,28 @@
 		<meta charset="utf-8" />
 		<title>Speedometer HTML5 Canvas</title>
 		<script src="speedometer.js"></script>
+		<script src="Balls.js"></script>
 	</head>
+	<body>
+    <canvas id='canvas'></canvas>
+    <style>
+    	body {
+    margin: 0;
+    padding: 0;
+    background-color: palegoldenrod;
+}
+
+#canvas {
+    display: flex;
+    background-color: #333333;
+    border: 1px solid black;
+    border-radius: 20px;
+    max-height: 800px;
+    max-width: 800px;
+    margin: 30px auto 0 auto;
+}
+</style>
+</body>
 	<body onload='draw(0);'>
 		<canvas id="tutorial" width="440" height="220">
 			Canvas not available.
