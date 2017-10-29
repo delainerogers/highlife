6cc69ac1e61898c995f2845e9f277894c8c8b634
Delaine Rogers <delainerogers@dhcp-wifi-8021x-155-41-76-76.bu.edu>
2017-10-29 09:25:31 -0400

    AH

--- header.php
+++ header.php
@@ -22,6 +22,24 @@
       .row.content {height: auto;} 
     }
   </style>
+
+<script language="Javascript" >
+function download(filename, text) {
+  var pom = document.createElement('a');
+  pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + 
+
+encodeURIComponent(text));
+  pom.setAttribute('download', filename);
+
+  pom.style.display = 'none';
+  document.body.appendChild(pom);
+
+  pom.click();
+
+  document.body.removeChild(pom);
+}
+</script>
+  
 </head>
 <body>
 
