b4d40574e549590447c36c6d98c48099a944917d
Delaine Rogers <delainerogers@dhcp-wifi-8021x-155-41-76-76.bu.edu>
2017-10-29 00:03:24 -0400

    fixed reflex test

--- reflextest.php
+++ reflextest.php
@@ -1,28 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <style>
-    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-    .row.content {height: 550px}
-    
-    /* Set gray background color and 100% height */
-    .sidenav {
-      background-color: #f1f1f1;
-      height: 100%;
-    }
-        
-    /* On small screens, set height to 'auto' for the grid */
-    @media screen and (max-width: 767px) {
-      .row.content {height: auto;} 
-    }
-  </style>
-</head>
+<?php include "header.php" ?>
+</div>
+</body>
 <body onload="startit()">
 
 <script language="JavaScript">
@@ -79,7 +57,7 @@ function stopTest()
 	{
 		if (!startPressed)
 		{
-			alert("press start first to start test");
+			//alert("press start first to start test");
 		}
 		else
 		{       
