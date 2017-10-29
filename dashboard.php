7c90d64c5e176e9d7d8982d353dd4d381bb2a2d9
rishab2113 <rishab_nayak@yahoo.com>
2017-10-29 02:39:40 -0400

    c

--- dashboard.php
+++ dashboard.php
@@ -5,35 +5,11 @@
 
 <h1>Dashboard</h1>
 
-	<!--
 	<head>
 		<meta charset="utf-8" />
 		<title>Speedometer HTML5 Canvas</title>
 		<script src="speedometer.js"></script>
-		<script src="Balls.js"></script>
 	</head>
--->
-	<body>
-    <canvas id='canvas'></canvas>
-    <style>
-    	body {
-    margin: 0;
-    padding: 0;
-    background-color: palegoldenrod;
-}
-
-#canvas {
-    display: flex;
-    background-color: #333333;
-    border: 1px solid black;
-    border-radius: 20px;
-    max-height: 800px;
-    max-width: 800px;
-    margin: 30px auto 0 auto;
-}
-</style>
-</body>
-	<!--
 	<body onload='draw(0);'>
 		<canvas id="tutorial" width="440" height="220">
 			Canvas not available.
@@ -45,5 +21,4 @@
 			</form>
 		</div>
 	</body>
--->
 </html>
